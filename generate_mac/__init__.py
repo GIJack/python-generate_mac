f880919192a4be1491244f16b2f406c9bb56ed9d
GI_Jack <GI_Jack@hackermail.com>
2020-11-04 14:59:21 -0500

    fixed error handling for _read_vid_file().

--- generate_mac/__init__.py
+++ generate_mac/__init__.py
@@ -32,7 +32,7 @@ class generate_mac():
             file_lines = in_file.readlines()
             in_file.close()
         except FileNotFoundError:
-            raise FileNotFoundError("ERROR: Cannot read VID file " + in_file)
+            raise FileNotFoundError("Cannot read VID file " + vid_file)
 
         # Sanitize inputs
         file_lines = generate_mac._strip_comments(file_lines)
@@ -195,20 +195,23 @@ class generate_mac():
     def vid_provided(vid_bytes):
         '''Generates only the Device bytes, given specified VID bytes'''
         test_vid = []
-        i = ""
         rand_bytes = ""
         output = ""
         ## Start with error checking
         #remove any trailing :
-        vid_bytes = vid_bytes.rstrip(":")
-        test_vid = vid_bytes.split(":")
+        try:
+            vid_bytes = vid_bytes.rstrip(":")
+            test_vid = vid_bytes.split(":")
+        except:
+            raise ValueError(str(vid_bytes) + ' are not valid VID bytes')
         # If there aren't precisely three bytes, its not a VID
         if len(test_vid) != 3:
             raise ValueError(vid_bytes + ' are not valid VID bytes')
-        for i in test_vid:
-            if generate_mac._is_byte(i) == False:
-                raise ValueError(vid_bytes + ' are not valid VID bytes')
-        
+        for byte in test_vid:
+            if generate_mac._is_byte(byte) == False:
+                raise ValueError(str(vid_bytes) + ' are not valid VID bytes')
+
+        # generate some new device bytes        
         rand_bytes = generate_mac._gen_rand_bytes(3)
         output = vid_bytes + ":" + rand_bytes
         return output
